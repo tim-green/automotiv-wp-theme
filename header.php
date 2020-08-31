2c32464548f5c89654989f42e6301be1336d864d
Tim <github@timgreen.ws>
2020-08-31 20:17:26 +1000

    added header to dsiplay <head> info and start #content

---
+++ header.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+
+<head>
+    <?php the_field('header_embed', 'option'); ?>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="https://gmpg.org/xfn/11">
+    <?php 
+    if (is_single() && !is_product()) {
+        article_structured_data($post->ID);
+    }
+    ?>
+    <?php wp_head(); 
