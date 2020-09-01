a4592532905e33f5bc3f28d06a990f77d5e89a42
Tim <github@timgreen.ws>
2020-09-01 21:46:06 +1000

    added copyright info

---
+++ footer.php
@@ -0,0 +1,7 @@
+    <div class="copyright bg-primary">
+        <div class="container">
+            <p class="text-white m-0 p-2 text-center">&copy; <?php wp_title(); ?> <?php echo date("Y"); ?></p>
+
+        </div>
+
+    </div>
