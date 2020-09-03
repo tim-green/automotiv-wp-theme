a5e6c268ee5036c8b8317204495ea252fe5015a7
Tim <github@timgreen.ws>
2020-09-03 12:23:12 +1000

    template part to display search

---
+++ template-parts/content-search.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Template part for displaying page content within page.php
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package automotiv
+ */
+
+?>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <div class="entry-content">
+    <?php
+    the_content();
+
+    wp_link_pages(
+        array(
+        'before' => '<div class="page-links">' . esc_html__('Pages:', 'automotiv'),
+        'after'  => '</div>',
+        ) 
+    );
+    ?>
+    </div><!-- .entry-content -->
+
+    <?php if (get_edit_post_link() ) : ?>
+        <footer class="entry-footer">
+        <?php
+        edit_post_link(
+            sprintf(
+                wp_kses(
+                    /* translators: %s: Name of current post. Only visible to screen readers */
+                    __('Edit <span class="screen-reader-text">%s</span>', 'automotiv'),
+                    array(
+                    'span' => array( 'class' => array(),),
+                    )
+                ),
+                get_the_title()
+            ),
+            '<span class="edit-link">',
+            '</span>'
+        );
+        ?>
+        </footer><!-- .entry-footer -->
+    <?php endif; ?>
+</article><!-- #post-<?php the_ID(); ?> -->
