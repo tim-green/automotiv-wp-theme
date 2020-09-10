8f894a037c9a0cc2fb8c30f9c9a208fc89762330
Tim <github@timgreen.ws>
2020-09-10 23:50:37 +1000

    created page template for FAQ page

---
+++ page-faq.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Template Name: FAQ Page
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package automotiv
+ */
+
+get_header();
+?>
+<div class="container">
+    <h1 class="py-3 mb-3 title-border"><?php the_title(); ?></h1>
+    <div class="row">
+        <div class="col-12">
+            <?php the_content();?>
+        </div>
+    </div>
+</div>
+
+
+
+
+<?php
+get_footer(); ?>
