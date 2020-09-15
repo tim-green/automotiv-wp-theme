afc1f0d5bb33d7bf02c78160adf41df27b2fceac
Tim <github@timgreen.ws>
2020-09-15 21:13:50 +1000

    related product

--- inc/woocommerce-functions.php
+++ inc/woocommerce-functions.php
@@ -21,3 +21,21 @@ if (!function_exists('change_woocommerce_order_number')) {
 }
 // cart - remove other shipping options if we have $4.50 shipping
 add_filter('woocommerce_package_rates', 'custom_shipping_option', 20, 2 );
+/**
+ * Related Products Args.
+ *
+ * @param array $args related products args.
+ * @return array $args related products args.
+ */
+function woocommerce_related_products_args($args)
+{
+    $defaults = array(
+        'posts_per_page' => 3,
+        'columns' => 3,
+    );
+
+    $args = wp_parse_args($defaults, $args);
+
+    return $args;
+}
+add_filter('woocommerce_output_related_products_args', 'woocommerce_related_products_args');
