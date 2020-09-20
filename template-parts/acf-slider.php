9838eb957c5cc443ca1b21d27ad12ccb9d38fe8d
Tim <github@timgreen.ws>
2020-09-20 16:11:05 +1000

    start of mark up including ACF for slider, using slick for slide functionality

--- template-parts/acf-slider.php
+++ template-parts/acf-slider.php
@@ -1,1 +1,49 @@
+<?php
+/**
  * Template part which is for ACF slider, which is display on the home page
+ *
+ *
+ * @package automotiv
+ */
+
+?>
+
+
+<div class="slider">
+    <div class="home-slider">
+    <?php if( have_rows('slider_images','option') ): ?>
+
+    <?php while( have_rows('slider_images','option') ): the_row(); 
+
+    // vars
+    $image = get_sub_field('image','option');
+    $text = get_sub_field('text','option');
+    $button_text = get_sub_field('button_text','option');
+    $button_link = get_sub_field('button_link','option');
+
+    ?>
+     <div class="slide" style="background-image: url(<?php echo $image['url']; ?>);">
+            <div class="slide-content d-flex align-items-center">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12 col-lg-6">
+                            <div class="slide-text text-center text-lg-left">
+                                <?php echo $text; ?>
+                                <a class="btn btn-primary"
+                                    href="<?php echo $button_link; ?>"><?php echo $button_text; ?></a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+        <?php endwhile; ?>
+        <?php endif; ?>
+
+    </div>
+   
+</div>
+
