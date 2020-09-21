da207c3914a424bdb58a70a143081f71872f7be8
Tim <github@timgreen.ws>
2020-09-21 11:31:30 +1000

    added custom markup for slider arrows - use of FontAwesome

---
+++ assets/js/plugins/plugins.js
@@ -0,0 +1,13 @@
+//var $ = jQuery;
+
+  jQuery(document).ready(function(){
+    jQuery('.home-slider').slick({
+        accessibility: true,
+        infinite: true,
+        arrows: true,
+        autoplay: false,
+        prevArrow: '<span class="motiv-slide-prev"><i class="fal fa-angle-left"></i></span>',
+        nextArrow: '<span class="motiv-slide-next"><i class="fal fa-angle-right"></i></span>',
+      });
+  });
+          
