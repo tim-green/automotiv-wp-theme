10aa896358eda007f9a5d4f09b67a35f9cd9ef0b
Tim <github@timgreen.ws>
2020-09-28 17:18:51 +1000

    updated grid system from 8 to 12

--- page-about.php
+++ page-about.php
@@ -13,7 +13,7 @@ get_header();
 <div class="container">
     <h1 class="py-3 mb-3 title-border"><?php the_title(); ?></h1>
     <div class="row">
-        <div class="col-12 col-lg-8">
+        <div class="col-12 col-lg-12">
             <?php
 		while ( have_posts() ) :
 			the_post();
