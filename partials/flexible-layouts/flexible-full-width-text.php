788a6ac00a8551b28af00f460bf7bb56884aa50f
Tim <github@timgreen.ws>
2020-09-28 13:38:11 +1000

    added full width text ACF flexible field

---
+++ partials/flexible-layouts/flexible-full-width-text.php
@@ -0,0 +1,13 @@
+<?php
+/*
+  ACF Flexible Content - Full Width Text (WYSIWYG)
+  
+  Field Example
+  <?php the_sub_field('text'); ?>
+*/
+?>
+<div class="row">
+   <div class="col-12 col-lg-12">
+            <?php the_sub_field('content'); ?>
+    </div>
+</div>
