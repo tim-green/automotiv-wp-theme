bdd082018f5f3258cdd045113ffbe0c84234989c
Tim <github@timgreen.ws>
2020-09-30 19:53:13 +1000

    display/retrieve the current post title

--- index.php
+++ index.php
@@ -18,6 +18,9 @@ get_header();
             <div class="col-12">
                 <header class="page-header">
                     <h1 class="page-title text-center"></h1>
+                    <h1 class="page-title text-center">
+						<?php the_title(); ?>
+					</h1>
                     <div class="archive-description text-center"></div>
                 </header><!-- .page-header -->
             </div>
