97511e23d9db3fc99e00bc2f2cbca320195be921
Tim <github@timgreen.ws>
2020-09-30 19:50:13 +1000

    template for displaying footer information

--- footer.php
+++ footer.php
@@ -1,3 +1,16 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package automotiv
+ */
+//get newsletter field from ACF
+$newsletter_form_id = get_field('footer_newsletter_signup_field_id', 'option'); 
+?>
     <div class="copyright bg-primary">
         <div class="container">
             <p class="text-white m-0 p-2 text-center">&copy; <?php wp_title(); ?> <?php echo date("Y"); ?></p>
