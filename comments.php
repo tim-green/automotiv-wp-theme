c41be067272c55446b0aced972fba021f1e93249
Tim <github@timgreen.ws>
2020-10-02 08:26:36 +1000

    start of the acutal comment function

--- comments.php
+++ comments.php
@@ -19,4 +19,26 @@ if ( post_password_required() ) {
 }
 ?>
 <div id="comments" class="comments-area">
+	<?php
+	// You can start editing here -- including this comment!
+	if ( have_comments() ) :
+		?>
+		<h2 class="comments-title">
+			<?php
+			$automotiv_comment_count = get_comments_number();
+			if ( '1' === $automotiv_comment_count ) {
+				printf(
+					/* translators: 1: title. */
+					esc_html__( 'One thought on &ldquo;%1$s&rdquo;', 'automotiv' ),
+					'<span>' . get_the_title() . '</span>'
+				);
+			} else {
+				printf( // WPCS: XSS OK.
+					/* translators: 1: comment count number, 2: title. */
+					esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $automotiv_comment_count, 'comments title', 'automotiv' ) ),
+					number_format_i18n( $automotiv_comment_count ),
+					'<span>' . get_the_title() . '</span>'
+				);
+			}
+			?>
 </div><!-- #comments -->
