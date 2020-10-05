ef84402e39bc66a2105445f10d4527ec2583eab5
Tim <github@timgreen.ws>
2020-10-05 11:40:49 +1100

    added two card block html structure

--- partials/flexible-layouts/flexible-two-card-block.php
+++ partials/flexible-layouts/flexible-two-card-block.php
@@ -6,3 +6,18 @@
   <?php the_sub_field('text'); ?>
 */
 ?>
+
+
+<div class="col-12 col-lg-6 mb-5">
+    <div class="card home-resource py-5">
+    </div>
+
+    <div class="card-body position-relative">
+    <h4 class="text-white text-center h2"></h4>
+
+    <p class="text-white text-center"></p>
+
+    <a href="" class="text-white text-center d-block mt-5"></a>
+    </div>
+    <div class="hero-overlay"></div>
+</div>
