d8e285b324f1d37b49f8fe837778be4e1a8bd8c2
Tim <github@timgreen.ws>
2020-10-05 11:43:24 +1100

    added based ACF repeater function

--- partials/flexible-layouts/flexible-two-card-block.php
+++ partials/flexible-layouts/flexible-two-card-block.php
@@ -7,6 +7,18 @@
 */
 ?>
 
+<!-- the repeater -->
+<?php if( have_rows('two_card_blocks') ): ?>
+
+<?php while( have_rows('two_card_blocks') ): the_row(); 
+
+// vars
+$bgimage = get_sub_field('background_image');
+$header = get_sub_field('header');
+$content = get_sub_field('content');
+$link = get_sub_field('link');
+
+?>
 
 <div class="col-12 col-lg-6 mb-5">
     <div class="card home-resource py-5">
@@ -21,3 +33,6 @@
     </div>
     <div class="hero-overlay"></div>
 </div>
+
+    <?php endwhile; ?>
+<?php endif; ?>
