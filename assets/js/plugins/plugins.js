0390c5aab86bb2c8fa096fea0aa6bc7c328f5793
Tim <github@timgreen.ws>
2020-10-06 23:58:17 +1100

    removed last comma

--- assets/js/plugins/plugins.js
+++ assets/js/plugins/plugins.js
@@ -7,7 +7,7 @@
         arrows: true,
         autoplay: false,
         prevArrow: '<span class="motiv-slide-prev"><i class="fal fa-angle-left"></i></span>',
-        nextArrow: '<span class="motiv-slide-next"><i class="fal fa-angle-right"></i></span>',
+        nextArrow: '<span class="motiv-slide-next"><i class="fal fa-angle-right"></i></span>'
       });
   });
           
