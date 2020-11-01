ebed2d17b6acc55738f515def74f97510acbddbd
Tim <github@timgreen.ws>
2020-11-01 19:37:38 +1100

    added new ACF field to display featured products from store

---
+++ partials/flexible-layouts/flexible-featured-products.php
@@ -0,0 +1,49 @@
+<?php
+/*
+  ACF Flexible Content - Feature 4 Products from Store
+  
+  Field Example
+  <?php the_sub_field('text'); ?>
+*/
+?>
+<div class="row">
+   <div class="col-12 col-lg-12">
+        
+   <div class="home-content pt-3 pb-5">
+        <?php
+		while ( have_posts() ) :
+			the_post();
+			//get_template_part( 'template-parts/content', 'page' );
+		endwhile; // End of the loop.
+        ?>
+    </div>
+
+
+    <?php
+    $args = array(
+        'post_type' => 'product',
+        'posts_per_page' => 4,
+        'tax_query' => array(
+                array(
+                    'taxonomy' => 'product_visibility',
+                    'field'    => 'name',
+                    'terms'    => 'featured',
+                ),
+            ),
+        );
+    $loop = new WP_Query( $args );
+    
+    if ( $loop->have_posts() ) {
+        echo '<div class="home-featured mb-5">';
+        echo '<h3>Featured Products</h3>';
+        echo '<p class="pb-3 mb-3 title-border">Here are some products hand picked by our staff</p>';
+        echo '<div class="row products">';
+        while ( $loop->have_posts() ) : $loop->the_post();
+            wc_get_template_part( 'content', 'product-homepage' );
+        endwhile;
+        echo '</div></div>';
+    }
+    wp_reset_postdata();
+    ?>
+    </div>
+</div>
