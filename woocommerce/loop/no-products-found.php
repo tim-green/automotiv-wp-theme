41654a1c62c86664f85e4d5d81e2d2accef0e57d
Tim <github@timgreen.ws>
2020-11-03 08:17:25 +1100

    added woocommerce template for displaying if not products been found.

--- woocommerce/loop/no-products-found.php
+++ woocommerce/loop/no-products-found.php
@@ -38,7 +38,7 @@ defined( 'ABSPATH' ) || exit;
     $loop = new WP_Query( $args );
     
     if ( $loop->have_posts() ) {
-        echo '<div class="home-featured mb-5">';
+        echo '<div class="store-featured mb-5">';
         echo '<h3>Featured Products</h3>';
         echo '<p class="pb-3 mb-3 title-border">Hand Picked by our staff.</p>';
         echo '<div class="row products">';
