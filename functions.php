7ed627b5c71e385102a3e88b8efe6fad6602b83d
Tim <github@timgreen.ws>
2020-11-09 17:29:54 +1100

    added pagination to match bootstrap layout

--- functions.php
+++ functions.php
@@ -560,7 +560,54 @@ $theme_version = '1.0.0';
 		require_once $custom_walker_footer;
 	}
 
-
+	/**
+	 * Pagination - numbering, using bootstrap
+	 *
+	 * @since v1.0
+	 */ 
+	function bootstrap_pagination( \WP_Query $wp_query = null, $echo = true, $params = [] ) {
+		if ( null === $wp_query ) {
+			global $wp_query;
+		}
+	
+		$add_args = [];
+		$pages = paginate_links( array_merge( [
+				'base'         => str_replace( 999999999, '%#%', esc_url( get_pagenum_link( 999999999 ) ) ),
+				'format'       => '?paged=%#%',
+				'current'      => max( 1, get_query_var( 'paged' ) ),
+				'total'        => $wp_query->max_num_pages,
+				'type'         => 'array',
+				'show_all'     => false,
+				'end_size'     => 3,
+				'mid_size'     => 1,
+				'prev_next'    => true,
+				'prev_text'    => __( '« Prev' ),
+				'next_text'    => __( 'Next »' ),
+				'add_args'     => $add_args,
+				'add_fragment' => ''
+			], $params )
+		);
+	
+		if ( is_array( $pages ) ) {
+			//$current_page = ( get_query_var( 'paged' ) == 0 ) ? 1 : get_query_var( 'paged' );
+			$pagination = '<div class="d-flex justify-content-center pagination"><ul class="pagination">';
+	
+			foreach ( $pages as $page ) {
+				$pagination .= '<li class="page-item' . (strpos($page, 'current') !== false ? ' active' : '') . '"> ' . str_replace('page-numbers', 'page-link', $page) . '</li>';
+			}
+	
+			$pagination .= '</ul></div>';
+	
+			if ( $echo ) {
+				echo $pagination;
+			} else {
+				return $pagination;
+			}
+		}
+	
+		return null;
+	}
+	
 	/**
 	 * Add additional classes on li menu
 	 *
