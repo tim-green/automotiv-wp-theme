65d03391cf7579bb8dff6e17043ad9896e0ca7e8
Tim <github@timgreen.ws>
2020-11-09 13:41:44 +1100

    updated theme location for footer menus

--- footer.php
+++ footer.php
@@ -11,6 +11,39 @@
 //get newsletter field from ACF
 $newsletter_form_id = get_field('footer_newsletter_signup_field_id', 'option'); 
 ?>
+                    <div class="col-4">
+                        <h6>Shop</h6>
+                        <?php wp_nav_menu(
+                            array(
+                                'theme_location' => 'footer-menu',
+                                'container' => 'ul',
+                                'menu_class' => 'nav flex-column',
+                                'depth' => 1,
+                            ));
+                        ?>
+                    </div>
+                    <div class="col-4">
+                        <h6>Account</h6>
+                        <?php wp_nav_menu(
+                            array(
+                                'theme_location' => 'footer-menu-1',
+                                'container' => 'ul',
+                                'menu_class' => 'nav flex-column',
+                                'depth' => 1,
+                            ));
+                        ?>
+                    </div>
+                    <div class="col-4">
+                        <h6>Resources</h6>
+                        <?php wp_nav_menu(
+                            array(
+                                'theme_location' => 'footer-menu-2',
+                                'container' => 'ul',
+                                'menu_class' => 'nav flex-column',
+                                'depth' => 1,
+                            ));
+                        ?>
+                    </div>
     <div class="copyright bg-primary">
         <div class="container">
             <p class="text-white m-0 p-2 text-center">&copy; <?php echo get_bloginfo('name'); ?> <?php echo date("Y"); ?></p>
