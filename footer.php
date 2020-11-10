428094486057d59edb235dfb9f3eaaac806033cb
Tim <github@timgreen.ws>
2020-11-11 08:28:35 +1100

    added copyright ACF field and markup to display newsletter

--- footer.php
+++ footer.php
@@ -11,6 +11,14 @@
 //get newsletter field from ACF
 $newsletter_form_id = get_field('footer_newsletter_signup_field_id', 'option'); 
 ?>
+
+</div><!-- #content -->
+
+<footer class="site-footer border-top mt-5">
+    <div class="container pt-5 pb-4">
+        <div class="row">
+            <div class="col-12 col-lg-8">
+                <div class="d-flex">
                     <div class="col-4">
                         <h6>Shop</h6>
                         <?php wp_nav_menu(
@@ -44,6 +52,27 @@ $newsletter_form_id = get_field('footer_newsletter_signup_field_id', 'option');
                             ));
                         ?>
                     </div>
+                </div>
+            </div>
+            <div class="col-12 col-lg-4">
+                <p mb-0><?php the_field('copyright_text','option'); ?></p>
+                <div class="mb-3 newsletter">
+                    <?php 
+                    if ($newsletter_form_id) {
+                        echo '<p class="mb-2 font-weight-bold">Get all the latest product updates, specials and coupons!</p>';
+                        gravity_form( $newsletter_form_id, false, false, false, '', true, 12 );
+                    }
+                     ?>
+                </div>
+            </div>
+        </div>
+        <div class="row pt-3">
+            <div class="col-12 text-center">
+               <!-- credit card info ACF field FA -->
+            </div>
+
+        </div>
+    </div>
     <div class="copyright bg-primary">
         <div class="container">
             <p class="text-white m-0 p-2 text-center">&copy; <?php echo get_bloginfo('name'); ?> <?php echo date("Y"); ?></p>
