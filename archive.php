65bd45cecf562fcc6f2cf77ae211d02441c8efef
Tim <github@timgreen.ws>
2020-11-11 08:29:29 +1100

    added padding top bootstrap class to header

--- archive.php
+++ archive.php
@@ -16,7 +16,7 @@ get_header();
 
         <?php if ( have_posts() ) : ?>
 
-        <div class="bg-light border-bottom mb-3">
+        <div class="bg-light border-bottom mb-3 pt-3">
             <div class="container py-3">
                 <div class="row align-items-center">
                     <div class="col-12">
@@ -30,6 +30,7 @@ get_header();
         </div>
         <div class="container">
             <div class="row row-cols-1 row-cols-md-3">
+           
                 <?php
 			/* Start the Loop */
 			while ( have_posts() ) :
@@ -39,8 +40,9 @@ get_header();
 				 * Include the Post-Type-specific template for the content.
 				 * If you want to override this in a child theme, then include a file called content-___.php (where ___ is the Post Type name) and that will be used instead.
 				 */
+                
 				get_template_part( 'template-parts/content', get_post_type() );
-
+                
 			endwhile;
 
 		else :
