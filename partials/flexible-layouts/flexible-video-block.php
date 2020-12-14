71b092bf1eab931c7298318d211b7e265fc1089b
Tim <github@timgreen.ws>
2020-12-14 18:15:10 +1100

    started with ACF repeater field for videos

--- partials/flexible-layouts/flexible-video-block.php
+++ partials/flexible-layouts/flexible-video-block.php
@@ -6,3 +6,19 @@
   <?php the_sub_field('text'); ?>
 */
 ?>
+    <?php if( have_rows('video') ): ?>
+
+    <?php while( have_rows('video') ): the_row(); 
+
+    // vars
+    $title = get_sub_field('video_title');
+    $video = get_sub_field('video_link');
+    ?>
+        <?php if( get_sub_field('video_title') ): ?>
+            <?php echo $title; ?>
+        <?php endif; ?>
+        <?php if( get_sub_field('video_link') ): ?>
+                <?php echo $video; ?>
+            <?php endif; ?>
+    <?php endwhile; ?>
+<?php endif; ?>
