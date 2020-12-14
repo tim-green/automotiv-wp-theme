ab86b48cc610f49c0f176ce4aec6efb2d2cfb6b3
Tim <github@timgreen.ws>
2020-12-14 18:15:43 +1100

    started bootstrap markup for display videos in card component

--- partials/flexible-layouts/flexible-video-block.php
+++ partials/flexible-layouts/flexible-video-block.php
@@ -6,6 +6,7 @@
   <?php the_sub_field('text'); ?>
 */
 ?>
+<div class="row">
     <?php if( have_rows('video') ): ?>
 
     <?php while( have_rows('video') ): the_row(); 
@@ -14,11 +15,25 @@
     $title = get_sub_field('video_title');
     $video = get_sub_field('video_link');
     ?>
+
+<div class="col-12 col-md-6 col-lg-6 mb-6">
+    <div class="card mb-4">
+        <div class="card-header bg-primary text-white">
         <?php if( get_sub_field('video_title') ): ?>
             <?php echo $title; ?>
         <?php endif; ?>
+        </div>
+        <div class="card-body embed-container" id="video-<?php echo get_row_index(); ?>">
         <?php if( get_sub_field('video_link') ): ?>
                 <?php echo $video; ?>
             <?php endif; ?>
+        </div>
+        
+    </div>
+
+</div>
+
+
     <?php endwhile; ?>
 <?php endif; ?>
+</div>
