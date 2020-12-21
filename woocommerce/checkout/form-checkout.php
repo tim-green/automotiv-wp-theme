f7bf6ba513376ee13d8ed937bbc10e2fef403bdf
Tim <github@timgreen.ws>
2020-12-21 17:45:30 +1100

    made form statement one line

--- woocommerce/checkout/form-checkout.php
+++ woocommerce/checkout/form-checkout.php
@@ -31,12 +31,11 @@ if (!$checkout->is_registration_enabled() && $checkout->is_registration_required
 <div class="row py-3 d-flex">
     <?php do_action('woocommerce_before_checkout_form', $checkout); ?>
 </div>
-<form name="checkout" method="post" class="checkout woocommerce-checkout"
-    action="<?php echo esc_url(wc_get_checkout_url()); ?>" enctype="multipart/form-data">
+
+<form name="checkout" method="post" class="checkout woocommerce-checkout" action="<?php echo esc_url(wc_get_checkout_url()); ?>" enctype="multipart/form-data">
     <div class="row">
         <div class="col-12 col-lg-7">
-            <?php if ($checkout->get_checkout_fields()) : ?>
-
+            
             <?php do_action('woocommerce_checkout_before_customer_details'); ?>
 
             <div class="row" id="customer_details">
